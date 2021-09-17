49acf8230a24ebc0b7e0941ae163177d7b1f20ea
Brenda Rodriguez <rodriguez.brenm@gmail.com>
2021-09-16 23:49:16 -0300

    add functionality to buttons to navigate the app

--- app/src/main/java/ar/teamrocket/duelosmeli/GameActivity.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/GameActivity.kt
@@ -1,5 +1,6 @@
 package ar.teamrocket.duelosmeli
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ar.teamrocket.duelosmeli.databinding.ActivityGameBinding
@@ -11,5 +12,11 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnExitGame.setOnClickListener { viewGameOver() }
+    }
+
+    fun viewGameOver() {
+        val intent = Intent(this, GameOverActivity::class.java)
+        startActivity(intent)
     }
 }
