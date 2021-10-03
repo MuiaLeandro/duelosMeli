839469b308c295c391f3fcf3a05d2ab743f25281
MuiaLeandro <lmuia@alumno.unlam.edu.ar>
2021-10-03 17:18:12 -0300

    Add mistake counter on GameActivity

--- app/src/main/java/ar/teamrocket/duelosmeli/GameActivity.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/GameActivity.kt
@@ -40,6 +40,7 @@ class GameActivity : AppCompatActivity() {
 
         binding.btnExitGame.setOnClickListener { viewGameOver(game) }
 
+        mistakeCounterUpdater(game)
         playGame(game)
     }
 
@@ -189,6 +190,13 @@ class GameActivity : AppCompatActivity() {
         binding.btnOption1.isClickable = false; binding.btnOption2.isClickable = false; binding.btnOption3.isClickable = false
         Handler(Looper.getMainLooper()).postDelayed({ colorResetter(); clearPrices() },1000)
         Handler(Looper.getMainLooper()).postDelayed({ actualGame = continuePlayChecker(actualGame) },1500)
+
+        mistakeCounterUpdater(game)
+    }
+
+    private fun mistakeCounterUpdater(game: Game) {
+        var mistakeCounter = getString(R.string.mistakeCounter, game.errors)
+        binding.tvMistakeCounter.text = mistakeCounter
     }
 
     private fun continuePlayChecker(game: Game): Game {
