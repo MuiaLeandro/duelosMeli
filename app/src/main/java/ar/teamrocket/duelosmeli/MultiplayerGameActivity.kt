11eb9b7cd7eb929f241e9d938fefd2ac7142c7c4
Brenda Rodriguez <rodriguez.brenm@gmail.com>
2021-10-11 05:12:08 -0300

    All views are added to the 'multiplayer_game', 'multiplayer_game_ready' and 'multiplayer_game_partial_result' activities

---
+++ app/src/main/java/ar/teamrocket/duelosmeli/MultiplayerGameActivity.kt
@@ -0,0 +1,11 @@
+package ar.teamrocket.duelosmeli
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MultiplayerGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multiplayer_game)
+    }
+}
---
+++ app/src/main/java/ar/teamrocket/duelosmeli/MultiplayerGameReadyActivity.kt
@@ -0,0 +1,11 @@
+package ar.teamrocket.duelosmeli
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MultiplayerGameReadyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multiplayer_game_ready)
+    }
+}
