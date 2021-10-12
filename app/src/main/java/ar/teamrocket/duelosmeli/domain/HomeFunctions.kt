45f77b872b7deeca6c029e561548b197836f15ac
Leandro Aranda <learanda@hotmail.com.ar>
2021-10-12 16:48:41 -0300

    I added gifs in 'activity_home' and override 'clearPrices()' function

---
+++ app/src/main/java/ar/teamrocket/duelosmeli/domain/HomeFunctions.kt
@@ -0,0 +1,9 @@
+package ar.teamrocket.duelosmeli.domain
+
+import android.content.Context
+import android.widget.ImageView
+
+interface HomeFunctions {
+
+    fun showImage(context: Context, image: Int, imageView: ImageView)
+}
