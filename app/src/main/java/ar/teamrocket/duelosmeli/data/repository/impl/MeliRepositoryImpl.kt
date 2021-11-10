9bd1d8a1ea4401a964a1ac6e743c0387bb42d8fc
Leandro Aranda <learanda@hotmail.com.ar>
2021-11-10 16:37:29 -0300

    I correct code for exception handling

--- app/src/main/java/ar/teamrocket/duelosmeli/data/repository/impl/MeliRepositoryImpl.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/data/repository/impl/MeliRepositoryImpl.kt
@@ -18,13 +18,14 @@ class MeliRepositoryImpl : MeliRepository {
 
         val response = API().getCategories()
 
-            when (response.code()) {
+            /*when (response.code()) {
                 in 200..299 -> return response.body()!!
                 400 -> throw BadRequestException(R.string.bad_request.toString())
                 404 -> throw NotFoundException(R.string.resource_not_found.toString())
                 in 500..599 -> throw InternalServerErrorException(R.string.server_error.toString())
                 else -> throw UnknownException(R.string.unknown_error.toString())
-            }
+            }*/
+        return response.body()!!
     }
 
     // Se obtiene un item de una categoría
@@ -32,13 +33,14 @@ class MeliRepositoryImpl : MeliRepository {
 
         val response = API().getArticlesFromCategory(id)
 
-            when (response.code()) {
+            /*when (response.code()) {
                 in 200..299 -> return response.body()!!
                 400 -> throw BadRequestException(R.string.bad_request.toString())
                 404 -> throw NotFoundException(R.string.resource_not_found.toString())
                 in 500..599 -> throw InternalServerErrorException(R.string.server_error.toString())
                 else -> throw UnknownException(R.string.unknown_error.toString())
-            }
+            }*/
+        return response.body()!!
     }
 
     // Se obtienen los datos más detallados de un artículo, por ahora usamos solo una imágen
@@ -46,12 +48,13 @@ class MeliRepositoryImpl : MeliRepository {
 
         val response = API().getArticle(id)
 
-            when (response.code()) {
+            /*when (response.code()) {
                 in 200..299 -> return response.body()!!
                 400 -> throw BadRequestException(R.string.bad_request.toString())
                 404 -> throw NotFoundException(R.string.resource_not_found.toString())
                 in 500..599 -> throw InternalServerErrorException(R.string.server_error.toString())
                 else -> throw UnknownException(R.string.unknown_error.toString())
-            }
+            }*/
+        return response.body()!!
     }
 }
--- app/src/main/java/ar/teamrocket/duelosmeli/domain/GameActivity.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/domain/GameActivity.kt
@@ -26,6 +26,7 @@ import ar.teamrocket.duelosmeli.domain.impl.GameFunctionsImpl
 import ar.teamrocket.duelosmeli.ui.viewmodels.GameViewModel
 import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Picasso
+import retrofit2.HttpException
 
 
 class GameActivity : AppCompatActivity() {
@@ -114,19 +115,23 @@ class GameActivity : AppCompatActivity() {
             })
             successChecker(correctPricePosition, game)
         })
-        vm.categoriesException.observe(this, {
-            Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
-        })
-        vm.itemFromCategoryException.observe(this, {
-            Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
-        })
-        vm.itemException.observe(this, {
-            Toast.makeText(this, it.toString(),Toast.LENGTH_LONG).show()
-        })
+        vm.categoriesException.observe(this, this::handleException)
+        vm.itemFromCategoryException.observe(this, this::handleException)
+        vm.itemException.observe(this, this::handleException)
         // Para probar un snackbar y ver diferencia con Toast
-        vm.itemException.observe(this, {
+        /*vm.itemException.observe(this, {
             Snackbar.make(binding.root, it.toString(), Snackbar.LENGTH_LONG).show()
-        })
+        })*/
+    }
+
+    private fun handleException(exception: Throwable?) {
+        if (exception is HttpException)
+            when (exception.code()) {
+                400 -> Toast.makeText(this, R.string.bad_request.toString(), Toast.LENGTH_LONG).show()
+                404 -> Toast.makeText(this, R.string.resource_not_found.toString(), Toast.LENGTH_LONG).show()
+                in 500..599 -> Toast.makeText(this, R.string.server_error.toString(), Toast.LENGTH_LONG).show()
+                else -> Toast.makeText(this, R.string.unknown_error.toString(), Toast.LENGTH_LONG).show()
+            }
     }
 
     private fun viewGameOver(game: Game) {
