ab59e3508e72ea34def5e1c81961859325bda52c
MuiaLeandro <lmuia@alumno.unlam.edu.ar>
2021-11-13 00:42:32 -0300

    add database by inject

--- app/src/main/java/ar/teamrocket/duelosmeli/MyApplication.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/MyApplication.kt
@@ -1,13 +1,23 @@
 package ar.teamrocket.duelosmeli
 
 import android.app.Application
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.room.Room
+import ar.teamrocket.duelosmeli.data.database.DuelosMeliDb
+import ar.teamrocket.duelosmeli.data.database.PlayerDao
+import ar.teamrocket.duelosmeli.ui.viewmodels.GameViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.KoinContext
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
 class MyApplication : Application() {
-    val appModule = module {
+    private val appModule = module {
+        single<PlayerDao> { getDatabase(get()).playerDao() }
+        viewModel<ViewModel> { GameViewModel() }
 
     }
 
@@ -20,4 +30,11 @@ class MyApplication : Application() {
         }
     }
 
+
+    private fun getDatabase(context: Context) = Room.databaseBuilder(
+        context,
+        DuelosMeliDb::class.java,
+        "duelosmeli-db")
+        .allowMainThreadQueries().build()
 }
+
--- app/src/main/java/ar/teamrocket/duelosmeli/domain/GameActivity.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/domain/GameActivity.kt
@@ -16,17 +16,21 @@ import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
 import androidx.core.content.res.ResourcesCompat
 import androidx.room.Room
+import androidx.room.RoomDatabase
 import ar.teamrocket.duelosmeli.ui.HomeActivity
 import ar.teamrocket.duelosmeli.R
 import ar.teamrocket.duelosmeli.data.repository.MeliRepository
 import ar.teamrocket.duelosmeli.data.repository.impl.MeliRepositoryImpl
 import ar.teamrocket.duelosmeli.data.database.DuelosMeliDb
+import ar.teamrocket.duelosmeli.data.database.PlayerDao
 import ar.teamrocket.duelosmeli.databinding.ActivityGameBinding
 import ar.teamrocket.duelosmeli.domain.impl.GameFunctionsImpl
 import ar.teamrocket.duelosmeli.ui.viewmodels.GameViewModel
 import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Picasso
+import org.koin.android.ext.android.inject
 import retrofit2.HttpException
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class GameActivity : AppCompatActivity() {
@@ -36,7 +40,8 @@ class GameActivity : AppCompatActivity() {
     lateinit var countDownTimer: CountDownTimer
     private var meliRepository: MeliRepository = MeliRepositoryImpl()
     private var gameFunctions: GameFunctions = GameFunctionsImpl()
-    private val vm: GameViewModel by viewModels()
+    private val vm: GameViewModel by viewModel()
+    val playerDao : PlayerDao by inject()
     var correctPricePosition: Int = 0
     lateinit var fake1: String
     lateinit var fake2: String
@@ -223,15 +228,16 @@ class GameActivity : AppCompatActivity() {
         if (game.errors == 3) {
             game.state = false
 
-            val database = Room.databaseBuilder(
-                applicationContext,
-                DuelosMeliDb::class.java,
-                "duelosmeli-db"
-            ).allowMainThreadQueries().build()
-            val playerDao = database.playerDao()
+
+//            val database = Room.databaseBuilder(
+//                applicationContext,
+//                DuelosMeliDb::class.java,
+//                "duelosmeli-db"
+//            ).allowMainThreadQueries().build()
+//            val playerDao = database.playerDao()
 
             //actualizar el jugador:
-            var player = playerDao.getById(game.playerId)
+            val player = playerDao.getById(game.playerId)
             if (player.isNotEmpty() && game.points > player[0].score ) {
                 player[0].score = game.points
                 playerDao.updatePlayer(player[0])
