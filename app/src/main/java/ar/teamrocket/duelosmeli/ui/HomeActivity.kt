4c4cb7dce64283b1cac69d036d99a076d6c3da8c
MuiaLeandro <lmuia@alumno.unlam.edu.ar>
2021-11-16 15:36:41 -0300

    Night mode disabled

--- app/src/main/java/ar/teamrocket/duelosmeli/ui/HomeActivity.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/ui/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import ar.teamrocket.duelosmeli.MainMenuActivity
 import ar.teamrocket.duelosmeli.R
 import ar.teamrocket.duelosmeli.databinding.ActivityHomeBinding
@@ -18,6 +19,8 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         // Reproducción del sonido
         val openSound = MediaPlayer.create(this, R.raw.open)
         openSound.start()
