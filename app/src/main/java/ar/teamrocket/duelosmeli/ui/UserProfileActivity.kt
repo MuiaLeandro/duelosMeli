465405a6eecf4b32fe10e26f5fa355d2a536b108
soleecito <soledadrotiroti@gmail.com>
2022-05-25 20:55:30 -0300

    modify dependencies

--- app/src/main/java/ar/teamrocket/duelosmeli/ui/UserProfileActivity.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/ui/UserProfileActivity.kt
@@ -1,13 +1,7 @@
-package ar.teamrocket.duelosmeli.ui
+package ar.teamrocket.duelosmeli.ui.userProfile
 
-import android.Manifest
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import ar.teamrocket.duelosmeli.databinding.ActivityUserProfileBinding
 
 class UserProfileActivity : AppCompatActivity() {
@@ -19,36 +13,5 @@ class UserProfileActivity : AppCompatActivity() {
         binding = ActivityUserProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCamera.setOnClickListener { requestPermission() }
     }
-
-    private fun requestPermission() {
-
-        when {
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                Toast.makeText(this, "Granted", Toast.LENGTH_SHORT).show()
-            }
-            ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                Manifest.permission.CAMERA
-            ) -> Toast.makeText(
-                this,
-                "Debe aceptar el permiso para el uso de la camara",
-                Toast.LENGTH_SHORT
-            ).show()
-            else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-        }
-
-    }
-
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted)
-                Toast.makeText(this, "GRANTED", Toast.LENGTH_SHORT).show()
-            else
-                Toast.makeText(this, "DENIED", Toast.LENGTH_SHORT).show()
-        }
 }
