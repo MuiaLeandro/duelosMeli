403a0cc79df5f8164ce03f7a5515519a52d6549f
Brenda Rodriguez <rodriguez.brenm@gmail.com>
2022-06-15 14:03:43 -0300

    Add functions in prefs to save location data.

--- app/src/main/java/ar/teamrocket/duelosmeli/data/preferences/Prefs.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/data/preferences/Prefs.kt
@@ -9,6 +9,11 @@ class Prefs(val context: Context) {
     }
     private val IS_FIRST_USE = "FIRST_USE"
     private val IS_LOCATION_ENABLED = "IS_LOCATION_ENABLED"
+    private val LOCATION_COUNTRY = "LOCATION_COUNTRY" // ARGENTINA
+    private val LOCATION_ADMIN = "LOCATION_ADMIN" // PROVINCIA DE BUENOS AIRES (in MELI filter is "state" but BS. AS. esta dividida por zonas o "interior")
+    private val LOCATION_SUB_ADMIN = "LOCATION_SUB_ADMIN" //LA MATANZA (in MELI filter is "city")
+    private val LOCATION_STATE_ID = "LOCATION_STATE_ID"
+    private val LOCATION_CITY_ID = "LOCATION_CITY_ID"
 
     private val storage = context.getSharedPreferences(SHARED_PREFERENCE_NAME,MODE_PRIVATE)
 
@@ -25,6 +30,36 @@ class Prefs(val context: Context) {
     }
     fun getLocationEnabled():Boolean{
         return storage.getBoolean(IS_LOCATION_ENABLED,false)
+    }
+    fun saveLocationCountry(country : String) {
+        storage.edit().putString(LOCATION_COUNTRY,country).apply()
+    }
+    fun getLocationCountry():String{
+        return storage.getString(LOCATION_COUNTRY,"") ?: ""
+    }
+    fun saveLocationState(state : String) {
+        storage.edit().putString(LOCATION_ADMIN,state).apply()
+    }
+    fun getLocationState():String{
+        return storage.getString(LOCATION_ADMIN,"") ?: ""
+    }
+    fun saveLocationCity(subAdmin : String) {
+        storage.edit().putString(LOCATION_SUB_ADMIN,subAdmin).apply()
+    }
+    fun getLocationCity():String{
+        return storage.getString(LOCATION_SUB_ADMIN,"") ?: ""
+    }
+    fun saveLocationStateId(state : String) {
+        storage.edit().putString(LOCATION_STATE_ID,state).apply()
+    }
+    fun getLocationStateId():String{
+        return storage.getString(LOCATION_STATE_ID,"") ?: ""
+    }
+    fun saveLocationCityId(subAdmin : String) {
+        storage.edit().putString(LOCATION_CITY_ID,subAdmin).apply()
+    }
+    fun getLocationCityId():String{
+        return storage.getString(LOCATION_CITY_ID,"") ?: ""
     }
 
 }
