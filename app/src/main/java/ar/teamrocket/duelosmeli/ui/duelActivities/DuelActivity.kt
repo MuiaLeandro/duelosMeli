bf5d9e950033b76cf4fc74a1abb7074f089b4ecd
Brenda Rodriguez <rodriguez.brenm@gmail.com>
2022-07-02 01:19:00 -0300

    shows the correct option and calculates the score

--- app/src/main/java/ar/teamrocket/duelosmeli/ui/duelActivities/DuelActivity.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/ui/duelActivities/DuelActivity.kt
@@ -1,23 +1,34 @@
 package ar.teamrocket.duelosmeli.ui.duelActivities
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.util.TypedValue
 import android.view.View
+import android.widget.Toast
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.core.content.res.ResourcesCompat
 import ar.teamrocket.duelosmeli.R
 import ar.teamrocket.duelosmeli.data.model.ItemDuel
 import ar.teamrocket.duelosmeli.databinding.ActivityDuelBinding
+import ar.teamrocket.duelosmeli.domain.GameFunctions
 import ar.teamrocket.duelosmeli.ui.MainMenuActivity
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DuelActivity : AppCompatActivity() {
     lateinit var binding: ActivityDuelBinding
     private val vm: DuelGameViewModel by viewModel()
+    private val gameFunctions: GameFunctions by inject()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,24 +36,102 @@ class DuelActivity : AppCompatActivity() {
         binding = ActivityDuelBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val gson = Gson()
-
-        //TODO: Hacer logica para leer los productos de la activity anterior y presentarlos en pantalla
         val itemsString = intent.getStringExtra("ITEMS")
         val itemsType = object : TypeToken<MutableList<ItemDuel>>() {}.type
         val items: MutableList<ItemDuel> = gson.fromJson(itemsString, itemsType)
         Log.d("ITEMS", items.toString())
 
-
         vm.initViewModel(items)
+
+        setListeners(items)
+        setObservers()
+    }
+
+    private fun setListeners(items: MutableList<ItemDuel>) {
+        binding.iHeaderDuel.ivButtonBack.setOnClickListener{ onBackPressed() }
         binding.btnOption1Duel.setOnClickListener {
-            if ((vm.countItem.value ?: 0) < items.size-1) {
-                vm.nextItem()
-            } else {
-                //TODO: Terminar partida
+            pressedOption(items, 1)
+        }
+        binding.btnOption2Duel.setOnClickListener {
+            pressedOption(items, 2)
+        }
+        binding.btnOption3Duel.setOnClickListener {
+            pressedOption(items, 3)
+        }
+    }
+
+    private fun pressedOption(items: MutableList<ItemDuel>,positionPressed:Int) {
+        optionIsChosen(positionPressed, vm.itemDuel.value?.correctPosition)
+        if ((vm.countItem.value ?: 0) < items.size - 1) {
+            Handler(Looper.getMainLooper()).postDelayed({ hideGame() }, 2000)
+        } else {
+            //TODO: Terminar partida
+            Handler(Looper.getMainLooper()).postDelayed({ viewFinishDuel() }, 2000)
+        }
+    }
+
+    private fun viewFinishDuel() {
+        Toast.makeText(this, "Tu puntuacion fue: ${vm.score.value}", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun hideGame() {
+        binding.clLoading.visibility = View.VISIBLE
+        vm.nextItem()
+    }
+
+    private fun optionIsChosen(pressedOption: Int, correctPosition: Int?) {
+        showCorrectOption(correctPosition, pressedOption)
+        if (correctPosition == pressedOption) {
+            vm.score.value = vm.score.value?.plus(10) ?: 10
+        }
+    }
+
+    private fun showCorrectOption(correctPosition: Int?, pressedOption: Int) {
+        when (correctPosition) {
+            1 -> {
+                binding.btnOption1Duel.setBackgroundColor(
+                    ResourcesCompat.getColor(
+                        resources, R.color
+                            .green1, null
+                    )
+                )
+            }
+            2 -> {
+                binding.btnOption2Duel.setBackgroundColor(
+                    ResourcesCompat.getColor(
+                        resources, R.color
+                            .green1, null
+                    )
+                )
+            }
+            else -> {
+                binding.btnOption3Duel.setBackgroundColor(
+                    ResourcesCompat.getColor(
+                        resources, R.color
+                            .green1, null
+                    )
+                )
             }
         }
-        binding.iHeaderDuel.ivButtonBack.setOnClickListener{ onBackPressed() }
-        setObservers()
+        if (correctPosition != pressedOption){
+            gameFunctions.optionsSounds(this,false)
+            when (pressedOption) {
+                1 -> { binding.btnOption1Duel
+                    .setBackgroundColor(ResourcesCompat.getColor(resources,
+                    R.color.red1,null))
+                }
+                2 -> { binding.btnOption2Duel
+                    .setBackgroundColor(ResourcesCompat.getColor(resources,
+                    R.color.red1,null))
+                }
+                else -> { binding.btnOption3Duel
+                    .setBackgroundColor(ResourcesCompat.getColor (resources,
+                    R.color.red1,null))
+                }
+            }
+        } else {
+            gameFunctions.optionsSounds(this,true)
+        }
     }
 
     private fun setObservers() {
@@ -52,6 +141,7 @@ class DuelActivity : AppCompatActivity() {
     }
 
     private fun loadUI(item: ItemDuel) {
+        colorReset()
         binding.tvProductNameDuel.text = item.title
         Picasso
             .get()
@@ -61,7 +151,7 @@ class DuelActivity : AppCompatActivity() {
             .into(binding.ivProductPictureDuel, object : Callback {
                 override fun onSuccess() {
                     //mostrar pantalla del juego
-                    binding.clLoading.visibility = View.GONE
+                    Handler(Looper.getMainLooper()).postDelayed({ showGame() }, 2000)
                 }
                 override fun onError(e: java.lang.Exception?) {
                     //do smth when there is picture loading error
@@ -85,6 +175,25 @@ class DuelActivity : AppCompatActivity() {
             }
             else -> println("Out of bounds")
         }
+    }
+
+    private fun showGame() {
+        binding.clLoading.visibility = View.GONE
+    }
+
+    private fun colorReset(){
+        binding.btnOption1Duel.setBackgroundColor(getColorFromAttr(R.attr.colorPrimary))
+        binding.btnOption2Duel.setBackgroundColor(getColorFromAttr(R.attr.colorPrimary))
+        binding.btnOption3Duel.setBackgroundColor(getColorFromAttr(R.attr.colorPrimary))
+    }
+    @ColorInt
+    fun Context.getColorFromAttr(
+        @AttrRes attrColor: Int,
+        typedValue: TypedValue = TypedValue(),
+        resolveRefs: Boolean = true
+    ): Int {
+        theme.resolveAttribute(attrColor, typedValue, resolveRefs)
+        return typedValue.data
     }
 
     override fun onBackPressed() {
