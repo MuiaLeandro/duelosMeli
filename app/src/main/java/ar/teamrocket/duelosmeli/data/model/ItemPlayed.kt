abc428ad138e2056019b987cbd9a4ed341bc22f8
soleecito <soledadrotiroti@gmail.com>
2022-07-02 19:25:17 -0300

    added data class item

---
+++ app/src/main/java/ar/teamrocket/duelosmeli/data/model/ItemPlayed.kt
@@ -0,0 +1,10 @@
+package ar.teamrocket.duelosmeli.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ItemPlayed (
+    var title: String,
+    var picture: String,
+): Parcelable
