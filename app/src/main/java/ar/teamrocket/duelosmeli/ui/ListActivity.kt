17b371319fad613f3918e806f865d11052d4ffe4
soleecito <soledadrotiroti@gmail.com>
2022-07-02 20:57:17 -0300

    visual aspects

--- app/src/main/java/ar/teamrocket/duelosmeli/ui/ListActivity.kt
+++ app/src/main/java/ar/teamrocket/duelosmeli/ui/ListActivity.kt
@@ -3,10 +3,13 @@ package ar.teamrocket.duelosmeli.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -31,8 +34,26 @@ class ListActivity : ComponentActivity() {
     }
 
     @Composable
+    fun cardList(lista: ArrayList<ItemPlayed>) {
+        LazyColumn(
+            contentPadding = PaddingValues(vertical = 8.dp, horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.background(Color.LightGray)
+        ) {
+            items(lista.toList()) { item ->
+                cardRow(
+                    itemPlayed = item
+                )
+            }
+        }
+    }
+
+    @Composable
     fun cardRow(itemPlayed: ItemPlayed) {
-        Card {
+        Card(
+            elevation = 4.dp,
+            shape = RoundedCornerShape(8.dp)
+        ) {
             Column(
                 Modifier
                     .padding(16.dp)
@@ -43,30 +64,24 @@ class ListActivity : ComponentActivity() {
                     textAlign = TextAlign.Center,
                     modifier = Modifier.fillMaxWidth()
                 )
+                Spacer(Modifier.size(8.dp))
+                Divider(
+                    color = Color.LightGray
+                )
                 AsyncImage(
                     model = itemPlayed.picture, contentDescription = null, modifier = Modifier
                         .fillMaxSize(),
                     contentScale = ContentScale.Crop
                 )
+                Divider(
+                    color = Color.LightGray
+                )
+                Spacer(Modifier.size(8.dp))
                 Text(
                     text = "Ver en Mercado Libre",
                     color = Color.Blue,
                     textAlign = TextAlign.Right,
                     modifier = Modifier.fillMaxWidth()
-                )
-            }
-        }
-    }
-
-    @Composable
-    fun cardList(lista: ArrayList<ItemPlayed>) {
-        LazyColumn(
-            contentPadding = PaddingValues(vertical = 8.dp, horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(lista.toList()) { item ->
-                cardRow(
-                    itemPlayed = item
                 )
             }
         }
